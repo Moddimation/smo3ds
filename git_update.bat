9ad132f63c393e77ff69edd6b29fd4fbcea2b2e4
vakhdmyt06 <vakhdmyt06@gmail.com>
2023-08-27 11:31:59 +0200

    test

--- git_update.bat
+++ git_update.bat
@@ -1,20 +1,27 @@
 @echo off
+if exist .gitmain\ (
+    set GITROOT=.gitmain
+) else (
+    set GITROOT=.git
+)
 echo Looking for remote updates...
-git --git-dir=.gitmain pull
+git --git-dir=%GITROOT% pull
 echo Adding changes...
-git --git-dir=.gitmain add .
+git --git-dir=%GITROOT% add .
 set /p DUMMY=enter update note: 
 echo Comitting changes...
-git --git-dir=.gitmain commit -m "%DUMMY%"
-echo Uploading changes, log: \"%DUMMY%\"
-git --git-dir=.gitmain push
+git --git-dir=%GITROOT% commit -m "%DUMMY%"
+echo Uploading changes, log: /"%DUMMY%/"
+git --git-dir=%GITROOT% push
 echo Looking for remote updates, finishing...
-git --git-dir=.gitmain pull
-git --git-dir=.gitmain fetch
+git --git-dir=%GITROOT% pull
+git --git-dir=%GITROOT% fetch
 
-git --git-dir=.gitbuild pull
-git --git-dir=.gitbuild add out.cci out.cci.xml out.cia
-git --git-dir=.gitbuild commit -m "by dima"
-git --git-dir=.gitbuild push
-git --git-dir=.gitbuild pull
-git --git-dir=.gitbuild fetch
+if exist .gitbuild\(
+    git --git-dir=.gitbuild pull
+    git --git-dir=.gitbuild add out.cci out.cci.xml out.cia
+    git --git-dir=.gitbuild commit -m "by dima"
+    git --git-dir=.gitbuild push
+    git --git-dir=.gitbuild pull
+    git --git-dir=.gitbuild fetch
+)
