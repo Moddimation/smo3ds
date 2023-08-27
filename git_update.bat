f3f0f988503117386e9bbe9a2f6021d61920189f
vakhdmyt06 <vakhdmyt06@gmail.com>
2023-08-27 09:18:36 +0200

    by dima

--- git_update.bat
+++ git_update.bat
@@ -1,6 +1,13 @@
-git pull
-git add .
-git commit -m "by dima"
-git push
-git pull
-git fetch
+git --git-dir=.gitmain pull
+git --git-dir=.gitmain add .
+git --git-dir=.gitmain commit -m "by dima"
+git --git-dir=.gitmain push
+git --git-dir=.gitmain pull
+git --git-dir=.gitmain fetch
+
+git --git-dir=.gitbuild pull
+git --git-dir=.gitbuild add out.cci out.cci.xml out.cia
+git --git-dir=.gitbuild commit -m "by dima"
+git --git-dir=.gitbuild push
+git --git-dir=.gitbuild pull
+git --git-dir=.gitbuild fetch
