c22abeab87b799be1d755e24bed8bf264821e42f
vakhdmyt06 <vakhdmyt06@gmail.com>
2023-11-06 16:51:18 +0100

    a few assets, mario and cap animations addition.

---
+++ Assets/Objects/Mario/MarioEvent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum plEvent
+{
+	Empty, // empty head? not moving.
+	Move, // mario controller event.
+	Demo, // just died
+}
+public class MarioEvent : MonoBehaviour {
+
+	public static plEvent myEvent;
+	public int myEventState = 0;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
